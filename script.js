606a19a3e86d34fe89ef559fd29fc162d1de78b8
Aala76 <aalazx@gmail.com>
2022-06-20 23:07:39 -0400

    Added color functions and edited style.css file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Declare global variables
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
+let colorSelected;
 
 // Add a row
 function addR() {
@@ -10,65 +10,65 @@ function addR() {
 
     row.insertCell(0);
     numRows += 1;
-    if(numRows ==1) numCols ++;
-    
+    if (numRows == 1) numCols++;
+
     if (numCols > 1) {
-        for (let i = 0; i < numCols -1 ; i++){
+        for (let i = 0; i < numCols - 1; i++) {
             row.insertCell(0);
         }
     }
-    console.log(numRows , 'rows and' , numCols, 'columns')
+    console.log(numRows, 'rows and', numCols, 'columns')
 
 }
 
 // Add a column
 function addC() {
-    if (numRows == 0){
+    if (numRows == 0) {
         addR();
     }
-    else{
+    else {
         let rows = document.querySelectorAll("tr");
-        for(let i = 0; i < numRows; i++){
-            
+        for (let i = 0; i < numRows; i++) {
+
             let cols = document.createElement("td");
             rows[i].appendChild(cols);
         }
         numCols += 1;
     }
 
-    console.log(numRows , 'rows and' , numCols, 'columns')
+    console.log(numRows, 'rows and', numCols, 'columns')
 }
 
 // Remove a row
 function removeR() {
-    if(numRows == 0)  alert('No Rows!');
-    else{
+    if (numRows == 0) alert('No Rows!');
+    else {
         let tab = document.getElementById("grid");
         tab.deleteRow(-1)
         numRows -= 1;
-        if(numRows == 0) numCols = 0;
+        if (numRows == 0) numCols = 0;
     }
-    console.log(numRows , 'rows and' , numCols, 'columns')
-    
+    console.log(numRows, 'rows and', numCols, 'columns')
+
 }
 
 // Remove a column
 function removeC() {
     if (numCols == 0) alert('No columns!');
-    else{
-        let row = document.querySelectorAll("tr"); 
-        for(let i = 0; i < numRows; i++){
-            row[i].deleteCell(-1); 
+    else {
+        let row = document.querySelectorAll("tr");
+        for (let i = 0; i < numRows; i++) {
+            row[i].deleteCell(-1);
         }
         numCols -= 1;
-        if (numCols == 0){
+        if (numCols == 0) {
             let tab = document.getElementById("grid");
             numRows = 0;
             tab.innerHTML = "";
         }
     }
-    
-    console.log(numRows + ' rows and' , numCols, 'columns')
+
+    console.log(numRows + ' rows and', numCols, 'columns')
 }
 
 // Set global variable for selected color
@@ -79,22 +79,40 @@ function selectColor(){
 
 // Fill all uncolored cells
 function fillU(){
-    alert("Clicked Fill All Uncolored"); // Replace this line with your code.
+    if (colorSelected == null) alert("Select a Color");
+    let cell = document.querySelectorAll("td"); 
+    for(let i = 0; i < cell.length; i++){
+        let col = window.getComputedStyle(cell[i]).backgroundColor;
+        if (col == 'rgb(255, 255, 255)'){ 
+            cell[i].style.backgroundColor = colorSelected;
+        }
+        
+    }
 }
 
 // Fill all cells
 function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
+    if (colorSelected == null) alert("Select a Color");
+    let cell = document.querySelectorAll("td"); 
+    for(let i = 0; i < cell.length; i++){
+        cell[i].style.backgroundColor = colorSelected;
+    }
 }
 
 // Clear all cells
 function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
+    
+    let cell = document.querySelectorAll("td"); 
+    for(let i = 0; i < cell.length; i++){
+        cell[i].style.backgroundColor = 'rgb(255, 255, 255)';
+
+    }
 }
 
-function deleteTab(){
+function deleteTab() {
     let tab = document.getElementById("grid");
     tab.innerHTML = "";
     numRows = 0;
     numCols = 0;
 }
+
